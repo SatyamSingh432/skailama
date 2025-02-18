5f4c91baf80ce375b7c853466051791c0e54a4e9
Satyam Singh <satyamrajput7239@gmail.com>
2025-02-18 22:27:59 +0530

    feat:Welcome Page left-side

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
-
+// import CreatePageHeader from "./components/createpage/CreatePageHeader";
+import WelcomePage from "./components/welcome/WelcomePage";
 function App() {
-  return <></>;
+  return (
+    <>
+      <WelcomePage />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/welcome/WelcomePage.jsx
@@ -0,0 +1,27 @@
+import "./WelcomePage.css";
+import whiteLogo from "../../assets/QuesLogo.svg";
+const WelcomePage = () => {
+  return (
+    <>
+      <div className="welcome-container">
+        <div className="about-container">
+          <div className="about-child-container">
+            <div className="design"></div>
+            <div className="about-container-logo">
+              <img src={whiteLogo} alt="" style={{ color: "green" }} />
+            </div>
+            <div className="about-title">
+              <h1>Your podcast will no longer be just a hobby.</h1>
+            </div>
+            <div className="about-info">
+              <p>Supercharge Your Distribution using our AI assistant!</p>
+            </div>
+          </div>
+        </div>
+        <div className="login-container"></div>
+      </div>
+    </>
+  );
+};
+
+export default WelcomePage;
