a4e59ed7123db971da31fd7d26ee9d471705c48d
Satyam Singh <satyamrajput7239@gmail.com>
2025-02-20 14:31:58 +0530

    feat: Implement Backend Register and Login

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,30 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
+
+import authRouter from "./routes/auth.js";
+import authMiddleware from "./middleware/authMiddleware.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on PORT ${process.env.PORT}`);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server listening on PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
+
+app.use(authRouter);
+
+app.get("/testauth", authMiddleware, (req, res) => {
+  const user = req.user;
+  res.json({ msg: "hello world", user });
 });
