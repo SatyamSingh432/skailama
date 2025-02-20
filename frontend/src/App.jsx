e094c731f63b3b16b1b103695ea438a6d0405d4b
Satyam Singh <satyamrajput7239@gmail.com>
2025-02-20 13:34:32 +0530

    chore: Move frontend to it's folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import WelcomePage from "./components/welcome/WelcomePage";
 import CreateProjectPage from "./components/createpage/CreateProjectPage";
 import ProjectPage from "./components/createpage/ProjectPage";
 import UploadPage from "./components/viewpage/UploadPage";
+
 function App() {
   return (
     <BrowserRouter>
